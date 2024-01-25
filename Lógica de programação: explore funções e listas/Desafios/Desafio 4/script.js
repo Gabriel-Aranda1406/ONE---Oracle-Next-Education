c693a33045195d0de93a4ecf662d88c44ebe2e16
Gabriel-Aranda1406 <biel.aranda234@gmail.com>
2024-01-24 22:36:58 -0300

    último desafio e finalização

---
+++ Lógica de programação: explore funções e listas/Desafios/Desafio 4/script.js
@@ -0,0 +1,38 @@
+//Desafio 1
+
+let listaGenerica = [];
+
+//Fim do desafio 1
+
+//Desafio 2
+
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+//Fim do desafio 2
+
+//Desafio 3
+
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+//Fim do desafio 3
+
+//Desafio 4
+
+let nomeHeroi = ['Ironman', 'Hulk', 'Thor'];
+console.log(nomeHeroi[0]);
+
+//Fim do desafio 4
+
+//Desafio 5
+
+let nomePersonagem = ['Tony Stark', 'Bruce Banner', 'Thor Odinson'];
+console.log(nomePersonagem[1]);
+
+//Fim do desafio 5
+
+//Desafio 6
+
+let nomeAleatorio = ['Tony', 'Thanos', 'Kang'];
+console.log(nomeAleatorio[2]);
+
+//Fim do desafio 6
